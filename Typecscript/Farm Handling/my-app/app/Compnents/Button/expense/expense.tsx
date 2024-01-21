b66f01827f561d5f5e484254fad8f8a51a370918
Alizarbukhari <alizar.fsd@gmail.com>
2024-01-21 09:49:48 +0500

    first commit

---
+++ Typecscript/Farm Handling/my-app/app/Compnents/Button/expense/expense.tsx
@@ -0,0 +1,14 @@
+import { expenseType } from "@/app/types/types";
+
+
+export default function Expense(expenses:expenseType) {
+  return (
+    <div>
+    note: "",
+    date:"" ,
+    amount:"",
+    category:"",
+    id :""
+    </div>
+  )
+}
---
+++ Typecscript/Farm Handling/my-app/app/expense/page.tsx
@@ -0,0 +1,10 @@
+import Expense from "../Compnents/Button/expense/expense";
+
+export default function page() {
+  return (
+    <div>
+        <Expense />
+    </div>
+  )
+}
+
